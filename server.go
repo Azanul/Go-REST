161f34713a1271102c31a72b9a1db9a500f60524
Azan <azanulhaque@gmail.com>
2021-08-09 01:22:41 +0530

    create and delete done

--- server.go
+++ server.go
@@ -3,40 +3,74 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-"log"
-"net/http"
+	"github.com/gorilla/mux"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
 )
 
 type Article struct {
-	Title string `json:"Title"`
-	Desc string `json:"desc"`
+	Id      string `json:"id"`
+	Title   string `json:"title"`
+	Desc    string `json:"desc"`
 	Content string `json:"content"`
 }
 
-var Articles []Article
+var Articles map[string]Article
 
-func homePage(w http.ResponseWriter, r *http.Request){
+func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the HomePage!")
 	fmt.Println("Endpoint Hit: homePage")
 }
 
 func handleRequests() {
-	http.HandleFunc("/", homePage)
+	myRouter := mux.NewRouter().StrictSlash(true)
 	fmt.Println("Served at 10000")
-	http.HandleFunc("/articles", returnAllArticles)
-	log.Fatal(http.ListenAndServe(":10000", nil))
+
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/all", returnAllArticles)
+	myRouter.HandleFunc("/article/{id}", returnOneArticle)
+	myRouter.HandleFunc("/create", createNewArticle).Methods("POST")
+	myRouter.HandleFunc("/article/{id}", deleteArticle).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
 
 func main() {
-	Articles = []Article{
-		Article{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
-		Article{Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+	Articles = map[string]Article{
+		"1": {Id: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+		"2": {Id: "2", Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
 	}
 
 	handleRequests()
 }
 
-func returnAllArticles(w http.ResponseWriter, r *http.Request){
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: returnAllArticles")
 	json.NewEncoder(w).Encode(Articles)
+}
+
+func returnOneArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+
+	fmt.Println("Endpoint Hit: returnOneArticle")
+	json.NewEncoder(w).Encode(Articles[key])
+}
+
+func createNewArticle(w http.ResponseWriter, r *http.Request) {
+	body, _ := ioutil.ReadAll(r.Body)
+	var article Article
+	json.Unmarshal(body, &article)
+	Articles[strconv.Itoa(len(Articles))] = article
+	fmt.Println("Endpoint Hit: createNewArticle")
+	fmt.Println(Articles)
+}
+
+func deleteArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+
+	fmt.Println("Endpoint Hit: deleteArticle")
+	delete(Articles, key)
 }
